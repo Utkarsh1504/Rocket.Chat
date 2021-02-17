6c7edbf633f37e4f8f6dc06db230aad441a05227
Matheus Barbosa Silva <36537004+matheusbsilva137@users.noreply.github.com>
2021-02-17 11:09:52 -0300

    Update ReplyTo_Enable field's label
    Co-authored-by: Kevin Aleman <kevin.aleman@rocket.chat>

--- app/lib/server/startup/email.js
+++ app/lib/server/startup/email.js
@@ -291,7 +291,7 @@ settings.addGroup('Email', function() {
 		this.add('Direct_Reply_ReplyTo_Enable', false, {
 			type: 'boolean',
 			env: true,
-			i18nLabel: 'Add Reply-To header',
+			i18nLabel: 'Add_ReplyTo_Header',
 		});
 		this.add('Direct_Reply_ReplyTo', '', {
 			type: 'string',
